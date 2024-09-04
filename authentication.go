cfcd0822ab14dd88bb5311f605be7ff5fc5a568d
StrikeW <wangsiyuanse@gmail.com>
2024-09-04 11:34:43 +0800

    fix

--- authentication.go
+++ authentication.go
@@ -1,7 +1,7 @@
 package mvc
 
 import (
-	"code.google.com/p/go.crypto/bcrypt"
+    "golang.org/x/crypto/bcrypt"
 	"database/sql"
 	"errors"
 	"fmt"
--- examples/webserver.go
+++ examples/webserver.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/travissimon/go-mvc"
+	"github.com/StrikeW/go-mvc"
 	"html/template"
 	"net/http"
 	"net/url"
